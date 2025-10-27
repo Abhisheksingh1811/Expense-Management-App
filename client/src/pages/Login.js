aa73665e65b175b0841a63f3002a334f67b4e08b
Abhisheksingh1811 <abhishsingh202@gmail.com>
2025-10-27 23:24:27 +0530

    Up

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      const { data } = await axios.post("https://expense-management-app-1p7f.onrender.com/api/v1/users/login", values);
+      const { data } = await axios.post("/api/v1/users/login", values);
       setLoading(false);
       message.success("login success");
       localStorage.setItem(
